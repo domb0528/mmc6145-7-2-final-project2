8b17c597c6c82920a51670eea05759ef1b5a9200
Dominick birge <dominickbirge@Dominicks-MacBook-Pro.local>
2023-08-05 16:48:11 -0600

    added a favorites page

---
+++ pages/favorites.jsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Calorie Watcher 🏋️‍♂️</title>
+        <meta name="description" content="Sip & Savor is a wine pairing app" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏋️‍♂️</text></svg>"/>
+      </Head>
+      <main>
+        <div className={styles.container}>
+          <h1>Welcome to Your Favorites Page Add Your Favorites Here.</h1>
+          <section className={styles.jumbo}>
+            <ul>
+              <li>
+                favorites go here
+              </li>
+            </ul>
+          </section>
+        </div>
+        <section className={styles.goSearch}>
+          <p></p>
+          <Link href="/search" className="button">Search for your suggested meals to add to your Favorites</Link>
+        </section>
+      </main>
+    </>
+  )
+}
