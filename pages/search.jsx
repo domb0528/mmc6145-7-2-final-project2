3e5d0ddcf291cf23a3c0d95cfb4774baa4ef500b
Dominick birge <dominickbirge@Dominicks-MacBook-Pro.local>
2023-08-07 02:26:20 -0600

    fixed api call

--- pages/search.jsx
+++ pages/search.jsx
@@ -52,10 +52,10 @@ export default function Search({winePairings, isLoggedIn}) {
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏋️‍♂️</text></svg>"/>
       </Head>
       <Header isLoggedIn={isLoggedIn}/>
-      <p className={styles.noResults}>Type in your nutrient goal numbers for calories or carbs etc, and the website will populate a meal recipies with all nutrient information.</p>
+      <p className={styles.noResults}>Type in your nutriant goal numbers for calories or carbs etc, and the website will populate a meal recipies with all nutriant information.</p>
 
       <form onSubmit={handleSubmit} className={styles.form}>
-        <label htmlFor="winePairing-search">Search by your amount of calories or nutrient values</label>
+        <label htmlFor="winePairing-search">Search by your amount of calories or nutriants values</label>
         <input
           value={query}
           onChange={e => setQuery(e.target.value)}
@@ -72,7 +72,7 @@ export default function Search({winePairings, isLoggedIn}) {
         {
         winePairings.map((winePairing, i) => (   
         
-        <WinePairingPreview key={i} id={winePairing.id} title={winePairing.title} image={winePairing.image} calories={winePairing.calories} carbs={winePairing.carbs} fat={winePairing.fat} protein={winePairing.protein}  />
+        <WinePairingPreview key={i} id={winePairing.id} title={winePairing.title} image={winePairing.image} calories={winePairing.calories} carbs={winePairing.carbs} fat={winePairing.fat} protein={winePairing.protein} addToFavorites={winePairing.addToFavorites} removeFromFavorites={winePairing.removeFromFavorites}    />
         ))}
         
         </section>
@@ -82,18 +82,61 @@ export default function Search({winePairings, isLoggedIn}) {
   )
 }
 
-function WinePairingPreview({id, title, image, calories, carbs, protein, fat}) {
+async function addToFavorites(WinePairingPreview) {
+  // TODO: use fetch to call POST /api/book
+  // Be sure to pass book in body (use JSON.stringify)
+  // Call router.replace(router.asPath) if you receive a 200 status
+  console.log
+
+  const res = await fetch('/util/winePairing', {
+    method:'POST', 
+    body: JSON.stringify({WinePairingPreview})
+  })
+  if (res.status === 200) {
+    console.log(await res.json())
+    console.log(res)
+    router.replace(router.asPath)
+  }
+
+
+}
+async function removeFromFavorites(WinePairingPreview) {
+  // TODO: use fetch to call DELETE /api/book
+  // Be sure to pass {id: <book id>} in body (use JSON.stringify)
+  // Call router.replace(router.asPath) if you receive a 200 status
+
+  const res = await fetch('/util/winePairing', {
+    method: 'DELETE', 
+    body: JSON.stringify({WinePairingPreview}),
+  })
+   // Call router.replace(router.asPath) if you receive a 200 status
+  if (res.status === 200) {
+  
+    router.replace(router.asPath)
+  }
+
+
+}
+
+
+
+function WinePairingPreview({id, title, image, calories, carbs, protein, fat, addToFavorites}) {
   return (
-    <div>
-    <Link href={'/favorites/'} className={styles.preview}>
+    <div className={styles.preview}>
       <Image src={image} width="231" height="231" alt={title}/>
       <span>{title}</span>
       <span>Calories: {calories}</span>
       <span>Total Carbs: {carbs}</span>
       <span>Total Protein: {protein}</span>
       <span>Total Fat: {fat}</span>
-
-    </Link>
+      
+      <Link href={'favorites'}>
+      <button onClick={addToFavorites}>
+                  Add to Favorites
+                </button></Link>
+      <button onClick={removeFromFavorites}>
+                  Remove from Favorites
+                </button>
     </div>
   )
 }
