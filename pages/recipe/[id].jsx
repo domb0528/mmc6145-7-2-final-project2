eb3f45292c3e936367c740159540fb80eec11b76
Dominick birge <dominickbirge@Dominicks-MacBook-Pro.local>
2023-08-09 22:43:38 -0600

    fixed deployment error test 3

--- pages/recipe/[id].jsx
+++ pages/recipe/[id].jsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import { getFood } from '../../util/food'
-import styles from '../../styles/food'
+import styles from '../../styles/Food.css'
 
 export async function getServerSideProps({params: {id}}) {
 
